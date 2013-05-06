2a0f6ef9e62575be512f03a62098d82b2e09288d
Thomas von Deyen <tvdeyen@gmail.com>
2013-05-06 14:35:01 +0200

    Disable ssl certificate verification for reporting errors

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,4 +1,7 @@
 Airbrake.configure do |config|
+  # disable ssl certificate verification
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
   config.api_key = ENV['ERRBIT_API_KEY']
   config.host    = ENV['ERRBIT_HOST']
   config.port    = 443
